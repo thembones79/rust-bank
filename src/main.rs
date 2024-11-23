690c8233f4a18423c02bae4e6803a021ccb85dad
thembones79 <michal.szumnarski@gmail.com>
2024-11-23 16:49:51 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+fn main() {
+    let bank = Bank::new();
+    println!("{:#?}", bank);
+}
